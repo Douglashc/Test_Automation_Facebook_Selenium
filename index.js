32707c21ee051845668b098bd7f74a548e8b362d
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-23 10:43:56 -0400

    Se realizo la automatizacion de radio y buton registrar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Builder, By, Key, until} = require("selenium-webdriver");
+const {Builder, By, Key, until, WebElement} = require("selenium-webdriver");
 require("chromedriver");
 
 async function TestFacebook()
@@ -10,8 +10,8 @@ async function TestFacebook()
     await driver.findElement(By.className('_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy')).click();
 
     await driver.manage().setTimeouts( { implicit: 1000} );
-    await driver.findElement(By.name("firstname")).sendKeys('Michael');
-    await driver.findElement(By.name("lastname")).sendKeys("Jackson");
+    await driver.findElement(By.name("firstname")).sendKeys('Maria');
+    await driver.findElement(By.name("lastname")).sendKeys("Galindo");
     
     await driver.findElement(By.name("reg_email__")).sendKeys("gokuh.dcz@gmail.com");
     await driver.findElement(By.name("reg_email_confirmation__")).sendKeys("gokuh.dcz@gmail.com");
@@ -22,7 +22,9 @@ async function TestFacebook()
     await driver.findElement(By.name("birthday_month")).sendKeys("6");
     await driver.findElement(By.name("birthday_year")).sendKeys("1997");
     
-    //await driver.findElement(By.css('#day > option[value=5]')).click();
+    await driver.findElement(By.name("sex")).click();
+
+    await driver.findElement(By.name("websubmit")).click();
     //await driver.findElement(By.name("q")).sendKeys("Selenium Test", Key.RETURN);
 }
 
