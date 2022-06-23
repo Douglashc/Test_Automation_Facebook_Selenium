839afbdc4c218c69628e2efe01225445e1404a82
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-22 21:00:54 -0400

    Se automatizo el click del boton CREAR CUENTA NUEVA

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ require("chromedriver");
 async function TestFacebook()
 {
     let driver = await new Builder().forBrowser("chrome").build();
+   
     await driver.get("https://www.facebook.com");
     
-    //await driver.findElement(By.name("q")).sendKeys("Selenium Test", Key.RETURN);
+    await driver.findElement(By.className('_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy')).click();
 }
 
 TestFacebook();
