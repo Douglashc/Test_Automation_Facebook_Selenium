312b2be6d14ccaeb87f4e1b743c2507e02e4f59a
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-22 22:38:13 -0400

    Los campos, dia, mes, anio de nacimiento fue automatizado
    Los campos descritos se llenaran de valores automaticamente

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ async function TestFacebook()
     await driver.findElement(By.className('_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy')).click();
 
     await driver.manage().setTimeouts( { implicit: 1000} );
-    await driver.findElement(By.name('firstname')).sendKeys('Michael');
+    await driver.findElement(By.name("firstname")).sendKeys('Michael');
     await driver.findElement(By.name("lastname")).sendKeys("Jackson");
     
     await driver.findElement(By.name("reg_email__")).sendKeys("gokuh.dcz@gmail.com");
     await driver.findElement(By.name("reg_email_confirmation__")).sendKeys("gokuh.dcz@gmail.com");
+
+    await driver.findElement(By.name("reg_passwd__")).sendKeys("123456dogs.");
     
+    await driver.findElement(By.name("birthday_day")).sendKeys("5");
+    await driver.findElement(By.name("birthday_month")).sendKeys("6");
+    await driver.findElement(By.name("birthday_year")).sendKeys("1997");
+    
+    //await driver.findElement(By.css('#day > option[value=5]')).click();
     //await driver.findElement(By.name("q")).sendKeys("Selenium Test", Key.RETURN);
 }
 
