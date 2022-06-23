9c5f3f04995c2df3e28e7034e70c319ee06b5211
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-22 22:00:20 -0400

    Campo nombre y campo apellido automatizado
    Los campos descritos se llenaran automaticamente

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Builder, By, Key, util} = require("selenium-webdriver");
+const {Builder, By, Key, until} = require("selenium-webdriver");
 require("chromedriver");
 
 async function TestFacebook()
@@ -8,6 +8,13 @@ async function TestFacebook()
     await driver.get("https://www.facebook.com");
     
     await driver.findElement(By.className('_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy')).click();
+
+    await driver.manage().setTimeouts( { implicit: 1000} );
+    await driver.findElement(By.name('firstname')).sendKeys('Michael');
+    await driver.findElement(By.name("lastname")).sendKeys("Jackson");
+    
+    
+    //await driver.findElement(By.name("q")).sendKeys("Selenium Test", Key.RETURN);
 }
 
 TestFacebook();
