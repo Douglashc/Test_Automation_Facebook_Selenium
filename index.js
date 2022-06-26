e9e3cf46ec7083be4be72d6846ca53f03fc296d6
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-25 22:32:47 -0400

    Assert hecho "Boton continuar"

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const {Builder, By, Key, until, WebElement} = require("selenium-webdriver");
+var assert = require("assert");
+
 require("chromedriver");
 
 async function TestFacebook()
@@ -10,21 +12,32 @@ async function TestFacebook()
     await driver.findElement(By.className('_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy')).click();
 
     await driver.manage().setTimeouts( { implicit: 1000} );
-    await driver.findElement(By.name("firstname")).sendKeys('Maria');
+    await driver.findElement(By.name("firstname")).sendKeys('Mario');
     await driver.findElement(By.name("lastname")).sendKeys("Galindo");
     
-    await driver.findElement(By.name("reg_email__")).sendKeys("gokuh.dcz@gmail.com");
-    await driver.findElement(By.name("reg_email_confirmation__")).sendKeys("gokuh.dcz@gmail.com");
+    await driver.findElement(By.name("reg_email__")).sendKeys("marioh@gmail.com");
+    await driver.findElement(By.name("reg_email_confirmation__")).sendKeys("marioh@gmail.com");
 
     await driver.findElement(By.name("reg_passwd__")).sendKeys("123456dogs.");
     
     await driver.findElement(By.name("birthday_day")).sendKeys("5");
     await driver.findElement(By.name("birthday_month")).sendKeys("6");
     await driver.findElement(By.name("birthday_year")).sendKeys("1997");
-    
-    await driver.findElement(By.name("sex")).click();
+
+    await driver.findElement(By.xpath("//label[contains(text(),'Hombre')]")).click();
 
     await driver.findElement(By.name("websubmit")).click();
+
+    await driver.manage().setTimeouts( { implicit: 9000} );
+    await driver.findElement(By.name("code")).sendKeys("5555");
+
+    await driver.findElement(By.name('confirm'))
+            .getText().then(textValue => {
+                assert.equal('Continuar', textValue)
+            });
+    
+    await driver.quit();
+
 
 }
 
